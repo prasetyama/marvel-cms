a863d62634da271d3e7aca5619490a05465be7c1
bagusrin <bagusrinn@gmail.com>
2017-10-18 18:16:59 +0700

    update app php

--- web/app.php
+++ web/app.php
@@ -1,6 +1,6 @@
 <?php
 
-//error_reporting(E_ALL); ini_set('display_errors', 1);
+error_reporting(E_ALL); ini_set('display_errors', 1);
 use Symfony\Component\Debug\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
