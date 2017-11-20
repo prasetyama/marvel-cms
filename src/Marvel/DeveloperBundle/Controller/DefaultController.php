41c7fdd60a6c09f38e88e8dcc87cdf67260573e9
bagusrin <bagusrinn@gmail.com>
2017-11-20 20:43:03 +0700

    try to bug fix server

--- src/Marvel/DeveloperBundle/Controller/DefaultController.php
+++ src/Marvel/DeveloperBundle/Controller/DefaultController.php
@@ -22,7 +22,7 @@ class DefaultController extends BaseController
 
         $this->init();
 
-        try {
+        //try {
 
             $act = new DeveloperManager($this->getDoctrine()->getManager());
             $res['data'] = $act->showAllDeveloper();
@@ -35,10 +35,9 @@ class DefaultController extends BaseController
 
             return $this->render('MarvelDeveloperBundle:Page:developer-list.html.twig',$res);
 
-        } catch (\Exception $e) {
-            return $this->errorResponse('Post developer failed, Please try again later', HttpStatusHelper::HTTP_PRECONDITION_FAILED);
-        }
-        return $this->render('MarvelDeveloperBundle:Page:developer-list.html.twig');
+        //} catch (\Exception $e) {
+          //  return $this->errorResponse('Post developer failed, Please try again later', HttpStatusHelper::HTTP_PRECONDITION_FAILED);
+        //}
     }
 
     public function addAction(){
